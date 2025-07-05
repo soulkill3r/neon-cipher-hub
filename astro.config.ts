d1e2dae95dcda5fcf336222277f5462b653aac87
Yacine S <hi@yacinesalhi.fr>
2025-07-05 18:10:33 +0200

    github pages

--- astro.config.ts
+++ astro.config.ts
@@ -32,8 +32,8 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
+  //adapter: vercel(),
+  //output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
   // 3. Local (standalone)
